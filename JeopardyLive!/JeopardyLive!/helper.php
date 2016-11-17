757b5e9ae5804a86794a9c2723293cef228c8ab3
philipnordmann <philip.nordmann@kings-mansion.com>
2016-11-17 11:51:27 +0100

    helper aktualisiert

--- JeopardyLive!/JeopardyLive!/helper.php
+++ JeopardyLive!/JeopardyLive!/helper.php
@@ -1,7 +1,7 @@
 <?php
-function createTile($link, $desc) {
+function createTile($link, $desc, $tileWidth) {
     ?>
-    <div class="tile tile-10 blue rounded shadow" onclick="location.href='<?php echo $link;?>';">
+    <div class="tile tile-<?php echo $tileWidth; ?> blue rounded shadow" onclick="location.href='<?php echo $link;?>';">
                 <div class="tile-content hvr-grow">
                     <a href="<?php echo $link;?>">
                         <p>
@@ -14,18 +14,20 @@ function createTile($link, $desc) {
 }
 
 function createAdd($link, $itemDesc) {
-    ?>
+?>
     <a href="#openModal" class="tile hvr-grow">
         <img src="resources/plus-icon-13062.png" height="50" width="50" />
     </a>
-    <div id="openModal" class="modalDialog">
+    <div id="openModal" class="modalDialog" >
         <div>
-            <a href="#close" title="Close" class="close">X</a>
-            <form action="<?php echo $link;?>" method="post" target="">
-                <input type="text" name="bezeichnung" class="insert" placeholder="Enter new <?php echo $itemDesc;?>" />
-                <input type="submit" class="insert" value="Add <?php echo $itemDesc;?>" />
-            </form>
-            <br />
+            <div class="innerAdd">
+                <a href="#close" title="Close" class="close">X</a>
+                <form action="<?php echo $link;?>" method="post" target="">
+                    <input type="text" name="bezeichnung" class="insert" placeholder="Enter new <?php echo $itemDesc;?>" />
+                    <input type="submit" class="insert" value="Add <?php echo $itemDesc;?>" />
+                </form>
+                <br />
+            </div>
         </div>
     </div>
 <?php
