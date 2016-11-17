24c2f89aa3b7948ae99b2d28600a85d23b4e1003
padbergprogramming <simon.padberg@kings-mansion.com>
2016-11-17 12:23:33 +0100

    j game rockt

--- JeopardyLive!/JeopardyLive!/JGameScreen.php
+++ JeopardyLive!/JeopardyLive!/JGameScreen.php
@@ -1,5 +1,6 @@
 <?php
 include("db.php");
+include("helper.php");
 echo file_get_contents( "template_game.html" );
 //if ($_POST) {
 //    echo '<pre>';
@@ -8,7 +9,7 @@ echo file_get_contents( "template_game.html" );
 //}
 
 //$thema = $_GET['thema'];
-//$katArray = $_GET['katArray'];
+//$katArray = $_POST['katArray'];
 
 
 /*
@@ -21,8 +22,8 @@ $kategorie6 = $katarray(6);
 */
 ?>
 <body>
-<table id ="table" style="width:100%">
-  <tr>
+    <table id="table" style="width:100%">
+        <!--  <tr>
     <th><div>kat1</div></th>
     <th><div>kat2</div></th>
   	<th><div>kat3</div></th>
@@ -42,22 +43,22 @@ $kategorie6 = $katarray(6);
   </tr>
 	<tr>
 		<td>
-            <div class="jeopardykastendings">200</div>
+            <div class="content">200</div>
 		</td>
 		<td>
-            <div class="jeopardykastendings">200</div>
+            <div class="content">200</div>
 		</td>
 		<td>
-            <div class="jeopardykastendings">200</div>
+            <div class="content">200</div>
 		</td>
 		<td>
-            <div class="jeopardykastendings">200</div>
+            <div class="content">200</div>
 		</td>
 		<td>
-            <div class="jeopardykastendings">200</div>
+            <div class="content">200</div>
 		</td>
 		<td>
-            <div class="jeopardykastendings">200</div>
+            <div class="content">200</div>
 		</td>
 	</tr>
 	<tr>
@@ -125,7 +126,39 @@ $kategorie6 = $katarray(6);
 
 </body>
 
+-->
 
-
+        <tr>
+            <th>kat1</th>
+            <th>kat2</th>
+            <th>kat3</th>
+            <th>kat4</th>
+            <th>kat5</th>
+            <th>kat6</th>
+        </tr>
+        <?php
 
  
+     for($j = 100; $j <= 500; $j=$j+100)
+     {
+        ?>
+        <tr>
+            <?php
+         for($i = 1; $i <= 6; $i++)
+     {
+            ?>
+                <td>
+
+                    <?php 
+         createTile("#",$j,"100 double-tile");
+                    ?>
+                </td>
+                <?php } ?>
+            </tr>
+            <?php
+ }
+            ?>
+
+</table>
+
+</body>
--- JeopardyLive!/JeopardyLive!/get_question.php
+++ JeopardyLive!/JeopardyLive!/get_question.php
@@ -1,3 +1,4 @@
 <?php
+$kategorie = $_POST("kategorie");
 
 ?>
