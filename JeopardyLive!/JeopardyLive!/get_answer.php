b071ea11718df4a69b009827235a47e2a3c2b15c
padbergprogramming <simon.padberg@kings-mansion.com>
2016-11-21 12:16:34 +0100

    get the f*cking answers

--- JeopardyLive!/JeopardyLive!/get_answer.php
+++ JeopardyLive!/JeopardyLive!/get_answer.php
@@ -10,4 +10,7 @@ $answer = get_answer(get_question_id($k_id,$points));
 
 _createQuestionTile("JGameScreen_next.php?id_done=".$k_id."&points_done=".$points."","",$answer,"50 double-tile");
 
+createTile("JGameScreen_next.php?id_done=".$k_id."&points_done=".$points."","gewusst!","50 tile");
+createTile("JGameScreen_next.php?id_done=".$k_id."&points_done=0","nicht gewusst!","50 tile");
+
 ?>
